ace0fd13e9967a2256ee90687939303d33d4358c
vleukhin <norad-2013@mail.ru>
2017-11-29 16:05:07 +0700

    верстка кнопок

---
+++ views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Просто подари</title>
+    <script src="/build/app.bundle.js"></script>
+    <link rel="stylesheet" href="/build/app.bundle.css">
+</head>
+<body>
+<div class="page">
+    <div class="content">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
--- views/landing.blade.php
+++ views/landing.blade.php
@@ -1,4 +1,11 @@
 {{--Created by Viktor Leukhin--}}
 {{--Tel: +7-926-797-5419--}}
 {{--E-mail: vleukhin@ya.ru--}}
-asdasd
+@extends('app')
+
+@section('content')
+    <button class="pp-button">Купить в один клик</button>
+    <button class="pp-button pp-button_bordered">Купить в один клик</button>
+    <button class="pp-button pp-button_green">Купить в один клик</button>
+    <button class="pp-button pp-button_green pp-button_bordered">Купить в один клик</button>
+@endsection
