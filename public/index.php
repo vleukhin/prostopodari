eb9afe184624e1c0f6823a38d350af89f49aa4f1
vleukhin <norad-2013@mail.ru>
2017-11-29 17:39:23 +0700

    fonts

--- public/index.php
+++ public/index.php
@@ -2,12 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (!empty($_POST['email'])) {
-    $file = __DIR__ . '/../storage/list.txt';
-    if ($email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        @file_put_contents($file, $email . PHP_EOL, FILE_APPEND);
-    }
-} else {
-    $blade = new \Jenssegers\Blade\Blade([__DIR__ . '/../views'], __DIR__ . '/../cache');
-    echo $blade->render('landing');
-}
+$blade = new \Jenssegers\Blade\Blade([__DIR__ . '/../views'], __DIR__ . '/../cache');
+echo $blade->render('landing');
