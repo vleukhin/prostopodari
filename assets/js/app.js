05b92691870f8b42a9530ffd63fe1bca1b88f009
vleukhin <norad-2013@mail.ru>
2017-11-29 14:24:03 +0700

    assets

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+import '../scss/app.scss';
+import 'font-awesome/scss/font-awesome.scss'
+import 'sweetalert/dist/sweetalert.css'
+
+window.$ = window.jQuery = require('jquery');
+window._ = require('lodash');
