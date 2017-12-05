7280ace0cfc9b9811abd0c89ad408ea39a1d2260
vleukhin <norad-2013@mail.ru>
2017-12-05 20:00:55 +0700

    заводим слим

--- public/index.php
+++ public/index.php
@@ -1,9 +1,24 @@
 <?php
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+define('ROOT_DIR', __DIR__ . '/../');
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv('../');
 $dotenv->load();
 
-$blade = new \Jenssegers\Blade\Blade([__DIR__ . '/../views'], __DIR__ . '/../cache');
-echo $blade->render('landing');
+$config = require '../config/app.php';
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+require '../app/lib/dependencies.php';
+
+$app->get('/', function (Request $request, Response $response) {
+    return $this->blade->render('landing');
+});
+
+$app->run();
