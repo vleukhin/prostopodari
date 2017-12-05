0ba602ba9454f9a8758cd434e14b6bce7b7c1762
vleukhin <norad-2013@mail.ru>
2017-12-05 19:37:18 +0700

    .env

--- public/index.php
+++ public/index.php
@@ -2,5 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv('../');
+$dotenv->load();
+
 $blade = new \Jenssegers\Blade\Blade([__DIR__ . '/../views'], __DIR__ . '/../cache');
 echo $blade->render('landing');
