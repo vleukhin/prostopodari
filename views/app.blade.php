6c602e83e98671eb7078a6e60955b38d946a597d
vleukhin <norad-2013@mail.ru>
2017-12-05 16:17:03 +0700

    img fix

--- views/app.blade.php
+++ views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Просто подари</title>
     <link rel="stylesheet" type="text/css" href="/js/slick/slick.css"/>
     <link rel="stylesheet" href="/build/app.bundle.css">
