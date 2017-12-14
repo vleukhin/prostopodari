13eec3b7108518d25d1537d25a7423e169527ce9
vleukhin <norad-2013@mail.ru>
2017-12-14 15:46:41 +0700

    чатра

--- views/app.blade.php
+++ views/app.blade.php
@@ -21,6 +21,21 @@
         }
         scrwidth > 0 && scrwidth < 669 ? window.DEVICE_TYPE = "phone" : scrwidth > 668 && scrwidth < 1025 ? window.DEVICE_TYPE = "tablet" : scrwidth > 1025 && (window.DEVICE_TYPE = "desktop");
     </script>
+    <!-- Chatra {literal} -->
+    <script>
+        (function(d, w, c) {
+            w.ChatraID = 'DNguSfZK7yGL3kgdq';
+            var s = d.createElement('script');
+            w[c] = w[c] || function() {
+                (w[c].q = w[c].q || []).push(arguments);
+            };
+            s.async = true;
+            s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+                + '//call.chatra.io/chatra.js';
+            if (d.head) d.head.appendChild(s);
+        })(document, window, 'Chatra');
+    </script>
+    <!-- /Chatra {/literal} -->
 </head>
 <body>
 @include('layout.counters')
